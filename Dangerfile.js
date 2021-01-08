ec4c0b6c9de1ee1e0e77f571b0acaa1c7f6672b3
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2021-01-08 16:20:48 +0100

    Azure pipeline config (#2)

---
+++ Dangerfile.js
@@ -0,0 +1,7 @@
+
+// import custom DangerJS rules
+// see http://danger.systems/js
+// see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
+const checkDangers = require('danger-plugin-digitalcitizenship').default;
+
+checkDangers();
