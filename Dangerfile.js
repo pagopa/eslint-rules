024150ee8cf764249733c1a2be5c15dbb68a3790
Daniele Manni <dan.manni@gmail.com>
2021-01-08 10:27:17 +0100

    Fix Dangerfile as js syntax

--- Dangerfile.js
+++ Dangerfile.js
@@ -2,6 +2,6 @@
 // import custom DangerJS rules
 // see http://danger.systems/js
 // see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
-import checkDangers from 'danger-plugin-digitalcitizenship';
+const checkDangers = require('danger-plugin-digitalcitizenship');
 
 checkDangers(); 
