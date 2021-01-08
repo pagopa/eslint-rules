abf95260903ec873f53155a2274717be5468a7c0
Daniele Manni <dan.manni@gmail.com>
2021-01-08 10:41:10 +0100

    Fix import default checkDangers fn

--- Dangerfile.js
+++ Dangerfile.js
@@ -2,6 +2,6 @@
 // import custom DangerJS rules
 // see http://danger.systems/js
 // see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
-const checkDangers = require('danger-plugin-digitalcitizenship');
+const checkDangers = require('danger-plugin-digitalcitizenship').default;
 
-checkDangers(); 
+checkDangers();
